bedb0ddf8cd7caa3c9aa3f1f7914a3542fdb375a
hasanyeniceli <97162068+hasanyeniceli@users.noreply.github.com>
2022-05-13 22:21:44 +0300

    day2-1.5

--- src/test/java/com/cydeo/test/day02_ocators/T5_getText_getAttribute.java
+++ src/test/java/com/cydeo/test/day02_ocators/T5_getText_getAttribute.java
@@ -1,7 +1,9 @@
 package com.cydeo.test.day02_ocators;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class T5_getText_getAttribute {
@@ -14,6 +16,19 @@ public class T5_getText_getAttribute {
 
         driver.get("https://practice.cydeo.com/registration_form");
 
+        WebElement headerText=driver.findElement(By.tagName("h2"));
+        //Expected: Registration form
+        String expectedHeaderText = "Registration form"; // we create variable to check it
+        String actualHeaderText=headerText.getText();
+        if (expectedHeaderText.equals(actualHeaderText)){
+            System.out.println("pass");
+        }else{
+            System.out.println("failed");
+        }
+        //4- Locate “First name” input box
+        // we are locating the web element using "name" locator
+        // name attribute has "firstname" value
+        //WebElement firstNameInput = driver.findElement(By.name("firstname"));
 
 
 
