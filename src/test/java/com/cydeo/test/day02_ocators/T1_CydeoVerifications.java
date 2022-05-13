f29965958b94c2d8b037b6103e942cbbe896d6a1
hasanyeniceli <97162068+hasanyeniceli@users.noreply.github.com>
2022-05-13 17:00:45 +0300

    day2-1.1

--- src/test/java/com/cydeo/test/day02_ocators/T1_CydeoVerifications.java
+++ src/test/java/com/cydeo/test/day02_ocators/T1_CydeoVerifications.java
@@ -6,19 +6,26 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class T1_CydeoVerifications {
     public static void main(String[] args) {
+        //TC #1: Cydeo practice tool verifications
+        //1. Open Chrome browser
         WebDriverManager.chromedriver().setup();
-        WebDriver driver=new ChromeDriver();
+        WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("https://practice.cydeo.com");
 
-        String expectedURL="cydeo";
-        String actualURL=driver.getTitle();
+        //2. Go to https://practice.cydeo.com
+        driver.navigate().to("https://practice.cydeo.com");
 
-        if (expectedURL.equals(actualURL)){
-            System.out.println( "passed");
+        //3. Verify URL contains
+        //Expected: cydeo
+        String expectedURL = "cydeo";
+        String actualURL = driver.getCurrentUrl();
+
+        if (actualURL.contains(expectedURL)){
+            System.out.println("URL verification PASSED!");
         }else{
-            System.out.println("failed");
+            System.out.println("URL verification FAILED!!!");
         }
+
 // 4. Verify title:
         //Expected: Practice
         String expectedTitle = "Practice";
