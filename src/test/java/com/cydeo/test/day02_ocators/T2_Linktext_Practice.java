e0892cd9179378987f17f8663b031d5841ba9b13
hasanyeniceli <97162068+hasanyeniceli@users.noreply.github.com>
2022-05-13 17:20:55 +0300

    day2-1.2

---
+++ src/test/java/com/cydeo/test/day02_ocators/T2_Linktext_Practice.java
@@ -0,0 +1,15 @@
+package com.cydeo.test.day02_ocators;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class T2_Linktext_Practice {
+    public static void main(String[] args) {
+        //TC #1: Cydeo practice tool verifications
+        //1. Open Chrome browser
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+}
