0f6d86579e61f760f815072267f12db68293479c
hasanyeniceli <97162068+hasanyeniceli@users.noreply.github.com>
2022-05-13 16:58:02 +0300

    day2-1

--- src/test/java/com/cydeo/test/day02_ocators/T1_CydeoVerifications.java
+++ src/test/java/com/cydeo/test/day02_ocators/T1_CydeoVerifications.java
@@ -9,7 +9,31 @@ public class T1_CydeoVerifications {
         WebDriverManager.chromedriver().setup();
         WebDriver driver=new ChromeDriver();
         driver.manage().window().maximize();
+        driver.get("https://practice.cydeo.com");
 
+        String expectedURL="cydeo";
+        String actualURL=driver.getTitle();
+
+        if (expectedURL.equals(actualURL)){
+            System.out.println( "passed");
+        }else{
+            System.out.println("failed");
+        }
+// 4. Verify title:
+        //Expected: Practice
+        String expectedTitle = "Practice";
+        String actualTitle = driver.getTitle();
+
+        if (actualTitle.equals(expectedTitle)){
+            System.out.println("Title verification PASSED!");
+        }else{
+            System.out.println("Title verification FAILED!!!");
+        }
+
+        driver.close();
+
+        //break 15mins: 10.10am cST
+        //break 15mins: 11.10am EST
 
     }
 
