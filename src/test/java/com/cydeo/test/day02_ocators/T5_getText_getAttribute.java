3db48c727e29c30923e1c5ac09d72510da4bc9db
hasanyeniceli <97162068+hasanyeniceli@users.noreply.github.com>
2022-05-13 22:10:35 +0300

    day2-1.5

---
+++ src/test/java/com/cydeo/test/day02_ocators/T5_getText_getAttribute.java
@@ -0,0 +1,21 @@
+package com.cydeo.test.day02_ocators;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class T5_getText_getAttribute {
+    public static void main(String[] args) {
+        //TC #1: Cydeo practice tool verifications
+        //1. Open Chrome browser
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://practice.cydeo.com/registration_form");
+
+
+
+
+    }
+}
