ffa11359cafe71e19dd75245b61fec44214b8a20
hasanyeniceli <97162068+hasanyeniceli@users.noreply.github.com>
2022-05-22 22:12:58 +0300

    day2-1.5.9.1

--- src/test/java/com/cydeo/test/day03_cssSelector_xpath/T4_cssSelector_getText.java
+++ src/test/java/com/cydeo/test/day03_cssSelector_xpath/T4_cssSelector_getText.java
@@ -1,4 +1,4 @@
-package com.cydeo.test;
+package com.cydeo.test.day03_cssSelector_xpath;
 
 import com.cydeo.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
@@ -9,6 +9,7 @@ public class T4_cssSelector_getText {
     public static void main(String[] args) {
         WebDriver driver= WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
+        driver.get("https://practice.cydeo.com/forgot_password");
         WebElement resetbuttonText=driver.findElement(By.cssSelector("button[value='Reset password']"));
         String actrestpass=resetbuttonText.getText();
         String exppas="Reset password";
