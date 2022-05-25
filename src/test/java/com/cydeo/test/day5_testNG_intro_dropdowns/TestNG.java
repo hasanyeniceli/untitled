e07e1cda735202d0759bd9d12ba88119aaeb588f
hasanyeniceli <97162068+hasanyeniceli@users.noreply.github.com>
2022-05-25 13:14:50 +0300

    day5.5

---
+++ src/test/java/com/cydeo/test/day5_testNG_intro_dropdowns/TestNG.java
@@ -0,0 +1,12 @@
+package com.cydeo.test.day5_testNG_intro_dropdowns;
+
+import org.testng.annotations.Test;
+
+public class TestNG {
+        @Test
+    public void test1(){
+            System.out.println("test1 is running");
+
+    }
+    }
+
