16543ea5b7701049f14ecd12ec3964e7f8e0b7ec
hasanyeniceli <97162068+hasanyeniceli@users.noreply.github.com>
2022-05-25 06:50:12 +0300

    day5.2

--- src/test/java/com/cydeo/test/day5_testNG_intro_dropdowns/T2_RadioButton.java
+++ src/test/java/com/cydeo/test/day5_testNG_intro_dropdowns/T2_RadioButton.java
@@ -1,7 +1,9 @@
 package com.cydeo.test.day5_testNG_intro_dropdowns;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import java.util.concurrent.TimeUnit;
 
@@ -15,8 +17,32 @@ public class T2_RadioButton {
         //2. Go to https://practice.cydeo.com/radio_buttons
         driver.get("https://practice.cydeo.com/radio_buttons");
 
+        WebElement radioButtonBlue=driver.findElement(By.cssSelector("input[id='blue']"));
+        System.out.println("radioButtonBlue.isDisplayed() = " + radioButtonBlue.isDisplayed());
+        System.out.println("radioButtonBlue.isSelected() = " + radioButtonBlue.isSelected());
+        WebElement radioButtonRed=driver.findElement(By.cssSelector("input[id='red']"));
+        System.out.println("radioButtonRed.isDisplayed() = " + radioButtonRed.isDisplayed());
+        System.out.println("radioButtonRed.isSelected() = " + radioButtonRed.isSelected());
+        WebElement radioButtonYellow=driver.findElement(By.cssSelector("input[id='yellow']"));
+        System.out.println("radioButtonYellow.isDisplayed() = " + radioButtonYellow.isDisplayed());
+        System.out.println("radioButtonYellow.isSelected() = " + radioButtonYellow.isSelected());
+        WebElement radioButtonBlack=driver.findElement(By.cssSelector("input[id='black']"));
+        System.out.println("radioButtonBlack.isDisplayed() = " + radioButtonBlack.isDisplayed());
+        System.out.println("radioButtonBlack.isSelected() = " + radioButtonBlack.isSelected());
+        WebElement radioButtonGreen=driver.findElement(By.cssSelector("input[id='green']"));
+        System.out.println("radioButtonGreen.isDisplayed() = " + radioButtonGreen.isDisplayed());
+        System.out.println("radioButtonGreen.isSelected() = " + radioButtonGreen.isSelected());
+        System.out.println("radioButtonGreen.isEnabled() = " + radioButtonGreen.isEnabled());
+        System.out.println("----------------------------------------------------------------------");
 
+        WebElement radioButtonHockey=driver.findElement(By.xpath("//input[@id='hockey']"));
+        radioButtonHockey.click();
 
+        if (radioButtonHockey.isSelected()){
+            System.out.println("Verification Pass Hockey Selected");
 
+        }else{
+            System.out.println("Verification Failed Hockey is Not Selected");
+        }
     }
 }
