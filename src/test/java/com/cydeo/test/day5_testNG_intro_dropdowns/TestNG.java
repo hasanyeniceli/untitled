74b82b6c756e86c57229e4121ea35689c844390c
hasanyeniceli <97162068+hasanyeniceli@users.noreply.github.com>
2022-05-25 13:38:04 +0300

    day5.6

--- src/test/java/com/cydeo/test/day5_testNG_intro_dropdowns/TestNG.java
+++ src/test/java/com/cydeo/test/day5_testNG_intro_dropdowns/TestNG.java
@@ -1,12 +1,37 @@
 package com.cydeo.test.day5_testNG_intro_dropdowns;
 
-import org.testng.annotations.Test;
+import org.testng.Assert;
+import org.testng.annotations.*;
 
 public class TestNG {
-        @Test
-    public void test1(){
-            System.out.println("test1 is running");
+    @BeforeClass
+    public void setupMehod(){
+        System.out.println(" BeforeClass is Running just once before all the methods in the class ");
+    }
+    @AfterClass
+    public void teardownMethod() {
+        System.out.println(" AfterClass is Running just once after all the methods in the class ");
+    }
+    @BeforeMethod
+        public void setUpMethod(){
+        System.out.println("Beforemethod is running");
+    }
+    @AfterMethod
+    public void tearDownMeMethod(){
+        System.out.println("---> AfterMethod is running");
+    }
+    @Test
+    public void test1() {
+        System.out.println("test1 is running");
+        // Assert Equals compares 2 of the same thing
+        String actual="apple";
+        String expected="apple";
+        Assert.assertEquals(actual,expected);
 
     }
+    @Test
+    public void test2() {
+        System.out.println("test2 is running");
     }
+}
 
