c372bbfbd2b78ad65819d485bfba3775e7f00987
hasanyeniceli <97162068+hasanyeniceli@users.noreply.github.com>
2022-05-26 08:56:19 +0300

    day07,1

--- src/test/java/com/cydeo/test/day7_webtables_utilities_javafaker/T1_WindowHandling.java
+++ src/test/java/com/cydeo/test/day7_webtables_utilities_javafaker/T1_WindowHandling.java
@@ -1,17 +1,20 @@
 package com.cydeo.test.day7_webtables_utilities_javafaker;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class T1_WindowHandling {
     public WebDriver driver;
 
     @BeforeMethod
-    public void setupMethod(){
+    public void setupMethod() {
         driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
@@ -19,9 +22,28 @@ public class T1_WindowHandling {
     }
 
     @Test
-    public void window_handling_test(){
+    public void window_handling_test() {
         //2. Go to: https://www.amazon.com
         driver.get("https://www.amazon.com");
+        //3. Copy-paste the lines from below into your class
+        ((JavascriptExecutor) driver).executeScript("window.open('https://google.com','_blank');");
+        ((JavascriptExecutor) driver).executeScript("window.open('https://etsy.com','_blank');");
+        ((JavascriptExecutor) driver).executeScript("window.open('https://facebook.com','_blank');");
 
+        //4. Create a logic to switch to the tab where Etsy.com is open
+        Set<String> alltheopenwindowsTabs=driver.getWindowHandles();
 
+        for (String each_window : alltheopenwindowsTabs) {
+                 driver.switchTo().window(each_window);
+            System.out.println("Current URL"+ driver.getCurrentUrl());
+            if (driver.getCurrentUrl().contains("etsy"));
+            break;
+
+        }
+// 5 Assert title contais ETSY Title
+        String actualtitle= driver.getTitle();
+        String expectedTitle="Etsy";
+        Assert.assertTrue(actualtitle.equals(expectedTitle),"not matching title");
+
+    }
 }
