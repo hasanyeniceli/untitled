9887ccf4f13479d7e280c2f0b39d748cd8fa227a
hasanyeniceli <97162068+hasanyeniceli@users.noreply.github.com>
2022-06-18 05:23:36 +0300

    day10.12

--- src/test/java/com/cydeo/test/day10_upload/T1_Registration_Form.java
+++ src/test/java/com/cydeo/test/day10_upload/T1_Registration_Form.java
@@ -5,6 +5,7 @@ import com.cydeo.utilities.Driver;
 import com.github.javafaker.Faker;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
 public class T1_Registration_Form {
@@ -35,7 +36,20 @@ inputName.sendKeys(faker.name().firstName());
         WebElement femaleGender=Driver.getDriver().findElement(By.xpath("//input[@value='female']"));
             femaleGender.click();
 
+        WebElement birhtday=Driver.getDriver().findElement(By.xpath("//input[@name='birthday']"));
+        birhtday.sendKeys("05/07/1985");
 
+        Select departmantdrpdown = new Select(Driver.getDriver().findElement(By.xpath("//select[@name='department']")));
+        departmantdrpdown.selectByIndex(faker.number().numberBetween(1,9));
+
+
+        Select job_titledrpdown = new Select(Driver.getDriver().findElement(By.xpath("//select[@name='job_title']")));
+        job_titledrpdown.selectByIndex(faker.number().numberBetween(1,8));
+
+        WebElement checkbox=Driver.getDriver().findElement(By.xpath("//input[@type='checkbox']"));
+        checkbox.click();
+        WebElement signbutton = Driver.getDriver().findElement(By.xpath("//button[@type='submit']"));
+        signbutton.click();
 
     }
 
